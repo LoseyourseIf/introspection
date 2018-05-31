f742b45ff406fdfd60b80bfe082f64b92a32478c
LoseyourseIf <15025160469@163.com>
2018-05-31 16:00:42 +0800

    堆排序

--- x-basic/src/main/java/xingyu/lu/review/basic/CommonlyUsedSorting.java
+++ x-basic/src/main/java/xingyu/lu/review/basic/CommonlyUsedSorting.java
@@ -2,6 +2,7 @@ package xingyu.lu.review.basic;
 
 /**
  * 常用排序算法
+ *
  * @author xingyu.lu
  * @create 2018-03-07 16:12
  **/
@@ -23,7 +24,7 @@ public class CommonlyUsedSorting {
 
     /**
      * @Describe 冒泡排序
-     * @Auther   xingyu.lu
+     * @Auther xingyu.lu
      */
     private void bubblingSorting(int[] arr) {
         int i, j;
@@ -38,46 +39,48 @@ public class CommonlyUsedSorting {
             }
         }
     }
+
     /**
      * @Describe 快速排序
-     * @Auther   xingyu.lu
+     * @Auther xingyu.lu
      */
-    private void quickSorting(int[] arr,int low,int high){
-            int i,j,temp,t;
-            if(low>high){
-                return;
-            }
-            i=low;
-            j=high;
-            //temp 基准位
-            temp = arr[low];
+    private void quickSorting(int[] arr, int low, int high) {
+        int i, j, temp, t;
+        if (low > high) {
+            return;
+        }
+        i = low;
+        j = high;
+        //temp 基准位
+        temp = arr[low];
 
-            while (i<j) {
-                //先看右边，依次往左递减
-                while (temp<=arr[j]&&i<j) {
-                    j--;
-                }
-                //再看左边，依次往右递增
-                while (temp>=arr[i]&&i<j) {
-                    i++;
-                }
-                if (i<j) {
-                    t = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = t;
-                }
+        while (i < j) {
+            //先看右边，依次往左递减
+            while (temp <= arr[j] && i < j) {
+                j--;
             }
-            //最后将基准为与i和j相等位置的数字交换
-            arr[low] = arr[i];
-            arr[i] = temp;
-            //递归调用左半数组
-            this.quickSorting(arr, low, j-1);
-            //递归调用右半数组
-            this.quickSorting(arr, j+1, high);
+            //再看左边，依次往右递增
+            while (temp >= arr[i] && i < j) {
+                i++;
+            }
+            if (i < j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        //递归调用左半数组
+        this.quickSorting(arr, low, j - 1);
+        //递归调用右半数组
+        this.quickSorting(arr, j + 1, high);
     }
+
     /**
      * @Describe 选择排序
-     * @Auther   xingyu.lu
+     * @Auther xingyu.lu
      */
     private void selectionSorting(int[] arr) {
         int i, j, index;
@@ -96,9 +99,10 @@ public class CommonlyUsedSorting {
         }
 
     }
+
     /**
      * @Describe 插入排序
-     * @Auther   xingyu.lu
+     * @Auther xingyu.lu
      */
     private void insertSorting(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
@@ -111,9 +115,10 @@ public class CommonlyUsedSorting {
             arr[j + 1] = temp;
         }
     }
+
     /**
      * @Describe 希尔排序
-     * @Auther   xingyu.lu
+     * @Auther xingyu.lu
      */
     private void shellSorting(int[] arr) {
         int gap = arr.length / 2;
@@ -138,10 +143,37 @@ public class CommonlyUsedSorting {
      * 其非ROOT节点的值均不大于（或不小于）
      * 其 leftChild 或 rightChild 节点的值
      * 其中 每个节点的值小于或等于其 leftChild、rightChild 的值 这样的堆称为小根堆
-     *      每个节点的值大于或等于其 leftChild、rightChild 的值 这样的堆称为大根堆
-     * @Auther   xingyu.lu
+     * 每个节点的值大于或等于其 leftChild、rightChild 的值 这样的堆称为大根堆
+     * @Auther xingyu.lu
      */
-    private void heapSorting(){
-
+    private void heapSorting(int[] a) {
+        int i;
+        for (i = a.length / 2 - 1; i >= 0; i--) {// 构建一个大顶堆
+            adjustHeap(a, i, a.length - 1);
+        }
+        for (i = a.length - 1; i >= 0; i--) {// 将堆顶记录和当前未经排序子序列的最后一个记录交换
+            int temp = a[0];
+            a[0] = a[i];
+            a[i] = temp;
+            adjustHeap(a, 0, i - 1);// 将a中前i-1个记录重新调整为大顶堆
+        }
     }
+
+    /**
+     * 构建大顶堆
+     */
+    private void adjustHeap(int[] a, int i, int len) {
+        int temp, j;
+        temp = a[i];
+        for (j = 2 * i; j < len; j *= 2) {// 沿关键字较大的孩子结点向下筛选
+            if (j < len && a[j] < a[j + 1])
+                ++j; // j为关键字中较大记录的下标
+            if (temp >= a[j])
+                break;
+            a[i] = a[j];
+            i = j;
+        }
+        a[i] = temp;
+    }
+
 }
