f94bb743104c3a1073858c03c8809472cdf0f586
LoseyourseIf <15025160469@163.com>
2018-06-11 16:58:36 +0800

    api

--- x-spring-boot-multiple-db/multiple-db-api/src/main/java/xingyu/lu/review/db/multiple/api/TestApi.java
+++ x-spring-boot-multiple-db/multiple-db-api/src/main/java/xingyu/lu/review/db/multiple/api/TestApi.java
@@ -2,13 +2,10 @@ package xingyu.lu.review.db.multiple.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import xingyu.lu.review.db.multiple.domain.master.User;
 import xingyu.lu.review.db.multiple.service.UserService;
 import xingyu.lu.review.tools.result.ResultModel;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author xingyu.lu
@@ -20,11 +17,9 @@ public class TestApi {
 
     @Resource
     private UserService userService;
+
     @RequestMapping("/test")
     public ResultModel test() {
-        List<User> users = new ArrayList<>();
-        users.add(userService.getMasterUserById(1));
-        users.add(userService.getClusterUserById(1));
-        return ResultModel.success(users);
+        return userService.getAllUser();
     }
 }
