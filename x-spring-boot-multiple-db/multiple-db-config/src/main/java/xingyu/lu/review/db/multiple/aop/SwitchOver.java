8ba688c0bfd33d7161001630410ec7b776983bd4
LoseyourseIf <15025160469@163.com>
2018-06-11 16:58:53 +0800

    conf

--- x-spring-boot-multiple-db/multiple-db-config/src/main/java/xingyu/lu/review/db/multiple/aop/SwitchOver.java
+++ x-spring-boot-multiple-db/multiple-db-config/src/main/java/xingyu/lu/review/db/multiple/aop/SwitchOver.java
@@ -1,5 +1,6 @@
 package xingyu.lu.review.db.multiple.aop;
 
+import org.springframework.core.annotation.Order;
 import xingyu.lu.review.db.multiple.config.DataSourceConfigurer;
 
 import java.lang.annotation.*;
@@ -8,10 +9,11 @@ import java.lang.annotation.*;
  * (╯‵□′)╯︵┻━┻
  * 数据源注解，用于设置数据源的 key，指定使用哪个数据源
  *
- * @author  xingyu.lu
- * @date    18/6/11 14:37
+ * @author xingyu.lu
+ * @date 18/6/11 14:37
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
+@Order(-1)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface SwitchOver {
