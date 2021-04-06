eaf44cd92d219ae24f9cf009c6ae053124e642c0
LoseyourseIf <15025160469@163.com>
2021-04-06 10:05:18 +0800

    基础题

---
+++ examples/xingyu-lab-written-test/src/main/java/xingyu/lu/lab/base/CollectionsDataType.java
@@ -0,0 +1,35 @@
+package xingyu.lu.lab.base;
+
+import java.util.*;
+
+/**
+ * 集合
+ *
+ * @author xingyu.lu
+ * @create 2018-04-19 10:04
+ **/
+public class CollectionsDataType {
+
+    private Collection collection;
+
+    private List list;
+    private ArrayList arrayList;
+    private LinkedList linkedList;
+
+    private Set set;
+    private TreeSet treeSet;
+    private HashSet hashSet;
+
+    private Queue queue;
+    private Stack stack;
+    private Vector vector;
+
+    private Map map;
+    private TreeMap treeMap;
+    private HashMap hashMap;
+    private Hashtable hashtable;
+    private LinkedHashMap linkedHashMap;
+    private WeakHashMap weakHashMap;
+    private IdentityHashMap identityHashMap;
+
+}
