919ceb77ad679169adeb378f81bf924f4e36f6ca
LoseyourseIf <15025160469@163.com>
2024-05-28 10:12:30 +0800

    remove useless

--- examples/xingyu-lab-ant-sofaboot/svc-provider/src/main/java/xingyu/lu/individual/svc/sofa/boot/provider/service/BootSofaService.java
+++
@@ -1,9 +0,0 @@
-package xingyu.lu.individual.svc.sofa.boot.provider.service;
-
-/**
- * @author xingyu.lu
- * @create 2020-12-23 10:25
- **/
-public interface BootSofaService {
-    String someService(String param);
-}
